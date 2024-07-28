2b3314026cd03dbb37dee770fba75512af437cb9
Bennet Bleßmann <bb-github@t-online.de>
2024-07-28 02:16:39 +0200

    Migrate to egui (#50)
    * update deps
    
    * initial migration from conrod piston to egui

--- game/src/game/color.rs
+++
@@ -1,5 +0,0 @@
-pub type Color = [f32; 4];
-
-pub const GREEN: Color = [0.0, 1.0, 0.0, 1.0];
-pub const RED: Color = [1.0, 0.0, 0.0, 1.0];
-pub const D_RED: Color = [0.227, 0.513, 0.678, 1.0];
--- game/src/gui.rs
+++ game/src/gui.rs
@@ -1,182 +1,108 @@
-use crate::game::GameState;
-use conrod_core::{
-    image::Id, image::Map, position::Positionable, position::Sizeable, text::GlyphCache, widget,
-    widget::Widget, widget_ids, Labelable, Ui, UiCell,
+use crate::game::{GameState, KeyMap};
+use eframe::egui;
+use std::{
+    fmt::{Debug, Display, Error, Formatter},
+    time::Duration,
 };
-use piston_window::{Graphics, PistonWindow, Window};
-use std::fmt::{Debug, Display, Error, Formatter};
-
-// Generate a unique `WidgetId` for each widget.
-widget_ids! {
-    pub struct Ids {
-        main_canvas,
-        menu_title,
-        level_buttons[],
-        level_selection_button,
-        editor_button,
-        contiue_button,
-        options_button,
-        back_button,
-        quit_button,
-    }
-}
-
-pub struct RenderContext<'font, G: Graphics> {
-    pub gl: G,
-    pub text_texture_cache: opengl_graphics::Texture,
-    pub text_vertex_data: Vec<u8>,
-    pub glyph_cache: GlyphCache<'font>,
-}
-
-pub struct GUI {
-    pub image_map: Map<opengl_graphics::Texture>,
-    pub image_ids: Vec<Id>,
-    pub ui: Ui,
-    pub ids: Ids,
-    pub active_menu: GUIVisibility,
-    pub fullscreen: bool,
-}
-
-#[allow(dead_code)]
-pub enum GUIVisibility {
-    //*NO GUI VISIBLE (ONLY GAME VISIBLE)
-    GameOnly(GameState),
-    //*INTERACTIVE MENU VISIBLE ON TOP OF GAME
-    //* E.g. Inventory, Pause Menu
-    OverlayMenu(MenuType, GameState),
-    //*ONLY MENU VISIBLE (NO GAME VISIBLE)
-    //* Main Menu, Level Selection, Options
-    MenuOnly(MenuType),
-}
-
-impl Debug for GUIVisibility {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        use self::GUIVisibility::*;
-        match self {
-            GameOnly(_) => Ok(()),
-
-            MenuOnly(menu) | OverlayMenu(menu, _) => Debug::fmt(&menu.menu_name(), f),
-        }
-    }
-}
-
-impl GUIVisibility {
-    pub fn handle_esc(&mut self, window: &mut PistonWindow) {
-        match self {
-            GUIVisibility::GameOnly(state) => {
-                if let GameState::GameOver { .. } = state {
-                    *self = GUIVisibility::MenuOnly(MenuType::Main)
-                } else {
-                    *self = GUIVisibility::OverlayMenu(MenuType::Pause, state.clone());
-                }
-            }
-            GUIVisibility::MenuOnly(menu_type) | GUIVisibility::OverlayMenu(menu_type, _) => {
-                let menu = menu_type.back();
-                if let Some(menu) = menu {
-                    *self = menu
-                } else {
-                    window.set_should_close(true);
-                }
-            }
-        }
-    }
-}
-
-impl Display for GUIVisibility {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        Debug::fmt(self, f)
-    }
-}
 
 #[derive(Debug)]
-pub enum MenuType {
-    Main,
-    Pause,
+pub enum Gui {
+    GameOnly(GameState),
+    PauseMenu(GameState),
+    MainMenu,
 }
 
-impl Display for MenuType {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        Debug::fmt(self, f)
-    }
-}
+impl eframe::App for Gui {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let (pressed_escape, f11_pressed, is_fullscreen) = ui.input(|ui| {
+                (
+                    ui.key_pressed(egui::Key::Escape),
+                    ui.key_pressed(egui::Key::F11),
+                    ui.viewport().fullscreen.unwrap_or_default(),
+                )
+            });
 
-pub trait Menu: Debug {
-    fn menu_name(&self) -> String;
+            if pressed_escape {
+                if !self.handle_esc() {
+                    ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
+                }
+            }
 
-    fn handle_input(&self);
+            if f11_pressed {
+                ui.ctx()
+                    .send_viewport_cmd(egui::ViewportCommand::Fullscreen(!is_fullscreen))
+            }
 
-    fn update(&self, ui: &mut UiCell, ids: &mut Ids) -> Box<dyn FnMut(&mut GUIVisibility) -> ()>;
+            match self {
+                Gui::GameOnly(game) => {
+                    let key_bindings = KeyMap {
+                        up: vec![egui::Key::ArrowUp, egui::Key::W],
+                        down: vec![egui::Key::ArrowDown, egui::Key::S],
+                        left: vec![egui::Key::ArrowLeft, egui::Key::A],
+                        right: vec![egui::Key::ArrowRight, egui::Key::D],
+                    };
 
-    fn back(&self) -> Option<GUIVisibility>;
-}
+                    for action in key_bindings.actions(ui) {
+                        game.perform(action);
+                    }
 
-impl Menu for MenuType {
-    fn menu_name(&self) -> String {
-        match self {
-            MenuType::Main => String::from("Main Menu"),
-            MenuType::Pause => String::from("Pause Menu"),
-        }
-    }
+                    game.step_time();
+                    ui.ctx().request_repaint_after(Duration::from_millis(20));
 
-    fn handle_input(&self) {
-        match self {
-            MenuType::Main => {}
-            MenuType::Pause => {}
-        }
-    }
+                    ui.add(game);
+                }
+                Gui::PauseMenu(game) => {
+                    ui.add(&mut *game);
+                    let game = game.clone();
 
-    fn update(&self, ui: &mut UiCell, ids: &mut Ids) -> Box<dyn FnMut(&mut GUIVisibility) -> ()> {
-        match self {
-            MenuType::Pause => {
-                widget::Text::new("Pause Menu")
-                    .font_size(30)
-                    .mid_top_of(ids.main_canvas)
-                    .set(ids.menu_title, ui);
-
-                let mut result: Box<dyn FnMut(&mut GUIVisibility) -> ()> = Box::new(|_| {});
-
-                for _press in widget::Button::new()
-                    .label("Continue")
-                    .label_font_size(30)
-                    .middle_of(ids.main_canvas)
-                    .padded_kid_area_wh_of(ids.main_canvas, ui.win_h / 4.0)
-                    .set(ids.contiue_button, ui)
-                {
-                    result = Box::new(|vis| match vis {
-                        GUIVisibility::OverlayMenu(_, game) => {
-                            *vis = GUIVisibility::GameOnly(game.clone())
-                        }
-                        _ => {}
+                    egui::Window::new("Pause Menu").show(ctx, |ui| {
+                        if ui.button("Continue").clicked() {
+                            *self = Gui::GameOnly(game);
+                        };
                     });
                 }
-                result
-            }
+                Gui::MainMenu => {
+                    ui.group(|ui| {
+                        ui.heading("Main Menu");
 
-            MenuType::Main => {
-                let mut result: Box<dyn FnMut(&mut GUIVisibility) -> ()> = Box::new(|_| {});
-
-                for _press in widget::Button::new()
-                    .label("Start Game")
-                    .middle_of(ids.main_canvas)
-                    .padded_kid_area_wh_of(ids.main_canvas, ui.win_h / 4.0)
-                    .set(ids.editor_button, ui)
-                {
-                    result = Box::new(|a| *a = GUIVisibility::GameOnly(GameState::new()));
+                        if ui.button("Start Game").clicked() {
+                            *self = Gui::GameOnly(GameState::new());
+                        };
+                    });
                 }
+            };
+        });
+    }
+}
 
-                widget::Text::new("Main Menu")
-                    .font_size(30)
-                    .mid_top_of(ids.main_canvas)
-                    .set(ids.menu_title, ui);
-                result
-            }
-        }
+impl Gui {
+    pub fn new() -> Self {
+        Gui::MainMenu
     }
 
-    fn back(&self) -> Option<GUIVisibility> {
+    pub fn handle_esc(&mut self) -> bool {
         match self {
-            MenuType::Main => None,
-            MenuType::Pause => Some(GUIVisibility::MenuOnly(MenuType::Main)),
+            Gui::GameOnly(state) => {
+                if let GameState::GameOver { .. } = state {
+                    *self = Gui::MainMenu
+                } else {
+                    *self = Gui::PauseMenu(state.clone());
+                }
+            }
+            Gui::PauseMenu(_) => {
+                *self = Gui::MainMenu;
+            }
+            Gui::MainMenu => {
+                return false;
+            }
         }
+        true
+    }
+}
+
+impl Display for Gui {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        Debug::fmt(self, f)
     }
 }
