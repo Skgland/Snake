3dd23dc6c8ebde44babb788b26fb1cdfee23c1d6
Bennet Bleßmann <bb-github@t-online.de>
2024-12-17 19:49:17 +0100

    make game more configurable and update dependencies

--- game/src/game.rs
+++ game/src/game.rs
@@ -47,13 +47,45 @@ impl std::ops::Add<Direction> for ObjectCoordinate {
 pub enum GameState {
     GameOver {
         score: usize,
+        settings: GameSettings,
     },
     GameState {
         apple: ObjectCoordinate,
         snake: VecDeque<ObjectCoordinate>,
         direction: Direction,
         next_step: Option<Instant>,
+        settings: GameSettings,
     },
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
+pub enum WallBehaviour {
+    #[default]
+    Death,
+    Loop,
+}
+impl WallBehaviour {
+    pub(crate) fn name(&self) -> &'static str {
+        match self {
+            WallBehaviour::Death => "Death",
+            WallBehaviour::Loop => "Loop",
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct GameSettings {
+    pub wall_behaviour: WallBehaviour,
+    pub size: [i8; 2],
+}
+
+impl Default for GameSettings {
+    fn default() -> Self {
+        Self {
+            wall_behaviour: Default::default(),
+            size: [32; 2],
+        }
+    }
 }
 
 impl GameState {
@@ -103,12 +135,20 @@ impl eframe::egui::Widget for &mut GameState {
     fn ui(self, ui: &mut eframe::egui::Ui) -> eframe::egui::Response {
         ui.centered_and_justified(|ui| {
             match self {
-                GameState::GameState { apple, snake, .. } => {
-                    const SIZE: f32 = GAME_SIZE as f32 * (TILE_SIZE + TILE_PADDING) + TILE_PADDING;
-                    let scale = ui.available_size().min_elem() / SIZE;
+                GameState::GameState {
+                    apple,
+                    snake,
+                    settings,
+                    ..
+                } => {
+                    let size_x: f32 =
+                        settings.size[0] as f32 * (TILE_SIZE + TILE_PADDING) + TILE_PADDING;
+                    let size_y: f32 =
+                        settings.size[0] as f32 * (TILE_SIZE + TILE_PADDING) + TILE_PADDING;
+                    let scale = (ui.available_width() / size_x).min(ui.available_height() / size_y);
 
                     let (_response, painter) = ui.allocate_painter(
-                        vec2(SIZE * scale, SIZE * scale),
+                        vec2(size_x * scale, size_y * scale),
                         Sense::focusable_noninteractive(),
                     );
 
@@ -135,17 +175,17 @@ impl eframe::egui::Widget for &mut GameState {
 
                     // draw background
                     painter.rect_filled(
-                        Rect::from_min_size(pos2(0.0, 0.0), vec2(SIZE * scale, SIZE * scale)),
+                        Rect::from_min_size(pos2(0.0, 0.0), vec2(size_x * scale, size_y * scale)),
                         Rounding::ZERO,
                         Color32::BLUE,
                     );
 
                     // draw corners
-                    for x in 1..GAME_SIZE {
+                    for x in 1..settings.size[0] {
                         let cx =
                             ((x as f32) * (TILE_SIZE + TILE_PADDING) + TILE_PADDING / 2.0) * scale;
 
-                        for y in 1..GAME_SIZE {
+                        for y in 1..settings.size[1] {
                             let cy = ((y as f32) * (TILE_SIZE + TILE_PADDING) + TILE_PADDING / 2.0)
                                 * scale;
                             painter.circle_filled(pos2(cx, cy), scale, Color32::BLACK);
@@ -156,8 +196,8 @@ impl eframe::egui::Widget for &mut GameState {
                         Rect::from_min_size(
                             pos2(TILE_PADDING / 2.0 * scale, TILE_PADDING / 2.0 * scale),
                             vec2(
-                                GAME_SIZE as f32 * (TILE_SIZE + TILE_PADDING) * scale,
-                                GAME_SIZE as f32 * (TILE_SIZE + TILE_PADDING) * scale,
+                                settings.size[0] as f32 * (TILE_SIZE + TILE_PADDING) * scale,
+                                settings.size[1] as f32 * (TILE_SIZE + TILE_PADDING) * scale,
                             ),
                         ),
                         Rounding::ZERO,
@@ -172,11 +212,11 @@ impl eframe::egui::Widget for &mut GameState {
                         draw_tile(&painter, segment, Color32::RED, scale);
                     }
                 }
-                GameState::GameOver { score } => {
+                GameState::GameOver { score, settings } => {
                     ui.heading(format!("Final length: {score}"));
 
                     if ui.button("Start New game").clicked() {
-                        *self = GameState::new()
+                        *self = GameState::new(settings.clone())
                     }
                 }
             }
@@ -185,74 +225,84 @@ impl eframe::egui::Widget for &mut GameState {
     }
 }
 
-pub fn delay_for_length(length: u32) -> Duration {
-    const MAX_LENGTH: u32 = GAME_SIZE as u32 * GAME_SIZE as u32;
+pub fn delay_for_length(length: u32, settings: &GameSettings) -> Duration {
     const MAX_DELAY: Duration = Duration::from_millis(250);
 
-    MAX_DELAY - MAX_DELAY * length / MAX_LENGTH
+    let max_length: u32 = settings.size[0] as u32 * settings.size[1] as u32;
+    MAX_DELAY - MAX_DELAY * length / max_length
 }
 
-pub fn new_snake() -> VecDeque<ObjectCoordinate> {
+pub fn new_snake(settings: &GameSettings) -> VecDeque<ObjectCoordinate> {
     vec![
         ObjectCoordinate {
-            x: GAME_SIZE / 2,
-            y: GAME_SIZE / 2,
+            x: settings.size[0] / 2,
+            y: settings.size[1] / 2,
         },
         ObjectCoordinate {
-            x: GAME_SIZE / 2,
-            y: GAME_SIZE / 2 + 1,
+            x: settings.size[0] / 2,
+            y: settings.size[1] / 2 + 1,
         },
         ObjectCoordinate {
-            x: GAME_SIZE / 2,
-            y: GAME_SIZE / 2 + 2,
+            x: settings.size[0] / 2,
+            y: settings.size[1] / 2 + 2,
         },
     ]
     .into()
 }
 
-pub fn generate_apple() -> ObjectCoordinate {
+pub fn generate_apple(settings: &GameSettings) -> ObjectCoordinate {
     use rand::distributions::{Distribution, Uniform};
 
-    let range: Uniform<i8> = Uniform::from(0..GAME_SIZE);
     let mut rng = ::rand::thread_rng();
 
-    let x: i8 = range.sample(&mut rng);
-    let y: i8 = range.sample(&mut rng);
+    let x: i8 = Uniform::from(0..settings.size[0]).sample(&mut rng);
+    let y: i8 = Uniform::from(0..settings.size[1]).sample(&mut rng);
     ObjectCoordinate { x, y }
 }
 
-pub fn wrap_position(mut obj: ObjectCoordinate) -> ObjectCoordinate {
-    while obj.x < 0 {
-        obj.x += GAME_SIZE;
-    }
-    if obj.x >= GAME_SIZE {
-        obj.x %= GAME_SIZE;
-    }
+pub fn new_position(
+    old_pos: ObjectCoordinate,
+    dir: Direction,
+    settings: &GameSettings,
+) -> Option<ObjectCoordinate> {
+    let mut obj = old_pos + dir;
 
-    while obj.y < 0 {
-        obj.y += GAME_SIZE;
+    match settings.wall_behaviour {
+        WallBehaviour::Death => {
+            if !(0..settings.size[0]).contains(&obj.x) || !(0..settings.size[1]).contains(&obj.y) {
+                None
+            } else {
+                Some(obj)
+            }
+        }
+        WallBehaviour::Loop => {
+            while obj.x < 0 {
+                obj.x += settings.size[0];
+            }
+            if obj.x >= settings.size[0] {
+                obj.x %= settings.size[0];
+            }
+
+            while obj.y < 0 {
+                obj.y += settings.size[1];
+            }
+            if obj.y >= settings.size[1] {
+                obj.y %= settings.size[1];
+            }
+            Some(obj)
+        }
     }
-    if obj.x >= GAME_SIZE {
-        obj.y %= GAME_SIZE;
-    }
-    obj
 }
-
-pub fn wall_death(obj: &ObjectCoordinate) -> bool {
-    !(0..GAME_SIZE).contains(&obj.x) || !(0..GAME_SIZE).contains(&obj.y)
-}
-
-//Controls whether touching the wall should kill or wrap around
-const DO_WALL_DEATH: bool = true;
 
 impl GameState {
-    pub fn new() -> GameState {
+    pub fn new(settings: GameSettings) -> GameState {
         GameState::GameState {
             // Rotation for the square.
-            apple: generate_apple(),
-            snake: new_snake(),
+            apple: generate_apple(&settings),
+            snake: new_snake(&settings),
             direction: Direction::UP,
             next_step: None,
+            settings,
         }
     }
 
@@ -262,44 +312,34 @@ impl GameState {
             snake,
             direction,
             next_step,
+            settings,
         } = self
         {
             let Some(next_step) = next_step else {
-                *next_step = Some(Instant::now() + delay_for_length(snake.len() as u32));
+                *next_step = Some(Instant::now() + delay_for_length(snake.len() as u32, &settings));
                 return;
             };
 
             if *next_step <= Instant::now() {
                 //spawn new head in current direction of previous head
                 let new_head = if let Some(head) = snake.front() {
-                    if !DO_WALL_DEATH {
-                        wrap_position(*head + *direction)
-                    } else {
-                        *head + *direction
-                    }
+                    let Some(pos) = new_position(*head, *direction, &settings) else {
+                        *self = GameState::GameOver {
+                            score: snake.len(),
+                            settings: settings.clone(),
+                        };
+                        return;
+                    };
+                    pos
                 } else {
-                    ObjectCoordinate { x: 0, y: 0 }
+                    unreachable!("Snake always has some body segments")
                 };
-
-                if DO_WALL_DEATH && wall_death(&new_head) {
-                    *self = GameState::GameOver { score: snake.len() };
-                    return;
-                }
-
-                //consume apple
-                if apple == &new_head {
-                    // generate new apple
-                    *apple = generate_apple();
-                } else {
-                    // no apple consumed remove last tail
-                    snake.pop_back();
-                }
 
                 //check if new_head collides with old tile
                 if snake.iter().any(|tile| tile == &new_head) {
-                    // +1 as tail has already been removed or apple eaten but new_head not yet added
                     *self = GameState::GameOver {
-                        score: snake.len() + 1,
+                        score: snake.len(),
+                        settings: settings.clone(),
                     };
                     return;
                 }
@@ -307,7 +347,16 @@ impl GameState {
                 //add new head
                 snake.push_front(new_head);
 
-                *next_step = Instant::now() + delay_for_length(snake.len() as u32);
+                // either consume an apple or remove the tail
+                if apple == &new_head {
+                    // generate new apple
+                    *apple = generate_apple(&settings);
+                } else {
+                    // no apple consumed remove last tail
+                    snake.pop_back();
+                }
+
+                *next_step = Instant::now() + delay_for_length(snake.len() as u32, &settings);
             }
         }
     }
@@ -315,4 +364,3 @@ impl GameState {
 
 pub const TILE_PADDING: f32 = 2.0;
 pub const TILE_SIZE: f32 = 10.0;
-pub const GAME_SIZE: i8 = 32;
